28c3ab3679a0c4ec41d313f00c7a36ad0ad3fa90
dimaswicakk <dimas.wicaksono12000@gmail.com>
2021-06-13 22:40:50 +0700

    Revisi 1

---
+++ homePage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Summer Camp | 2021</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="styleCSS.css">
+        <link rel="stylesheet" href="./bootstrap-5.0.0-beta3-dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        
+        <nav class="container navbar navbar-expand-sm bg-light navbar-light pl-3">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="landingPage.html">Landing Page</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="homePage.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            </ul>
+        </nav>
+
+        <div class="container" style="margin-top: 10px">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h2>About Us</h2>
+                    <img src="./image/aboutus.jpg" height="200px" alt="Forest">
+                    <p>
+                        Ketahui tentang kami lebih lanjut. <br>
+                        <a href="aboutUs.php">Klik disini</a>
+                    </p>
+                </div>
+                <div class="col-sm-8">
+                    <h2>Judul</h2>
+                    <img src="./image/home1.jpg" height="200px" alt="Forest">
+                </div>
+            </div>
+        </div>
+
+        <script src="./bootstrap-5.0.0-beta3-dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
