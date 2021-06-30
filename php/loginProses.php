79b5bb849a898c0b4e1a72f1afc575b3807a83ee
salsariyadi <72984514+salsariyadi@users.noreply.github.com>
2021-06-30 21:18:26 +0700

    Update loginProses.php

--- php/loginProses.php
+++ php/loginProses.php
@@ -1,18 +1,30 @@
 <?php
-    include "connection.php";
 
-    $username = $_POST['username'];
-    $password = md5($_POST['password']);
+include 'connection.php';
 
-    $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($connect, $query);
-    $row = mysqli_fetch_assoc($result);
+            if(isset($_POST['submit'])){
 
-    if($row['level'] == 1){
-        echo readfile("homeAdmin.php");
-    } elseif($row['level'] == 2){
-        echo readfile("homeUser.php");
-    } else{
-        echo mysqli_error($connect);
-    }
-?>
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                $datacheck = mysqli_query($connect, "SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+                $result = mysqli_fetch_array($datacheck);
+                $status = $result['status'];
+                $login_user = $result['username'];
+                $row = mysqli_num_rows($datacheck);
+
+                //cek kondisi login
+                if($row > 0){
+                    session_start();
+                    $_SESSION['login_user'] = $login_user;
+
+                    if ($status == 'user'){
+                        header('location: homeUser.php');
+                    }elseif ($status == 'admin'){
+                        header('location: homeAdmin.php');
+                    }else{
+                        header('location: login.php');
+                    }
+                }
+            }
+            ?>
