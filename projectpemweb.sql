c53680324ae0ea0b6031d6b098753d6929b4f061
salsariyadi <72984514+salsariyadi@users.noreply.github.com>
2021-06-30 23:27:59 +0700

    Update projectpemweb.sql

--- projectpemweb.sql
+++ projectpemweb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2021 at 10:30 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Generation Time: Jun 30, 2021 at 08:59 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,21 +42,20 @@ CREATE TABLE `tour` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `level` varchar(5) NOT NULL
+  `id_user` int(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `status` enum('admin','user','','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
-(0, 'guest@email.com', '123', '2'),
-(124200034, 'husenm52@email.com', '3a5da0246045797d0ebdafff345eb541', '1'),
-(124200052, 'salsabl.r@email.com', 'fd0e41545086c7f294bb0c1fc19e4aeb', '1'),
-(124200053, 'dimaswicakk@email.com', 'e1eeab8aac7ef2fdce3c0515b0037a41', '1');
+INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `status`) VALUES
+(14, 'user1', 'user1234', 'user1@gmail.com', 'user'),
+(19, 'admin1, 'admin1234', 'admin1@gmail.com', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +71,7 @@ ALTER TABLE `tour`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -83,6 +82,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `tour`
   MODIFY `id_tour` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
